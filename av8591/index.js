ad2588137c7e93837b085dbf56e98854060103df
wei <wrenth04@gmail.com>
2017-10-06 01:09:42 +0800

    fix img size

--- av8591/index.js
+++ av8591/index.js
@@ -3,6 +3,6 @@ const data = require('./data.json');
 data.feed.entry.forEach(function(d) {
   const title = d.link[2].title;
   const url = d.link[2].href.replace('http', 'https');
-  const img = d['media$thumbnail'].url.replace('s72c', 's1600');
+  const img = d['media$thumbnail'].url.replace('s72-c', 's1600');
   console.log([url, img, title].join(' '));
 });
