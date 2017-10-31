a457aa402a9c29567afc411912f3a1a04141d46a
Phil Huang <wrenth04@gmail.com>
2017-11-01 01:45:11 +0800

    ignore file

--- av8591/api.js
+++ av8591/api.js
@@ -6,6 +6,7 @@ data.items.forEach(function(d) {
   const title = d.title;
   const url = d.url.replace('http', 'https');
   const content = d.content;
+  if(content.indexOf('/file/d/') == -1) return;
   const img = content.split('src=\"')[1].split('\" style')[0];
   const fileId = content.split('/file/d/')[1].split('/preview')[0];
   console.log([url, img, fileId,title].join(' '));
