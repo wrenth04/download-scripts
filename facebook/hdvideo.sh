2a41546c0f8282548b080a7453bb08b30f762457
Phil Huang <wrenth04@gmail.com>
2017-11-16 23:43:24 +0800

    fb 1080p

--- facebook/hdvideo.sh
+++ facebook/hdvideo.sh
@@ -3,25 +3,22 @@
 #https://www.facebook.com/will.fans/videos/1727290680633402/
 url=$1
 
-html=$(wget -U Mozilla -q -O - "$url")
+html=$(wget -U "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36" -q -O - "$url")
 
-vhtml=$(echo "$html" | grep "video:")
-video=${vhtml#*video:}; video=${video#*url:\"}; video=${video%%\"*}
 thtml=$(echo "$html" | grep "pageTitle")
 title=${thtml#*pageTitle\">}; title=${title%%<*}
-ahtml=$(echo "$html" | grep "audio:")
-audio=${ahtml#*audio:[}; audio=${audio#*url:\"}; audio=${audio%%\"*}
-
 echo "download $title"
 
-r=$RANDOM
-echo $audio
-wget -U Mozilla --referer "$url" -O "video.$r.mp4" "$video"
-wget -U Mozilla --referer "$url" -O "audio.$r.aac" "$audio"
+for q in 1080p 720p 480p; do
+  x=$(echo "$html" | grep "\"$q")
+  if [ "x$x" = "x" ]; then continue; fi
 
-ffmpeg -y -nostdin -i "video.$r.mp4" -i "audio.$r.aac" -c copy "$title.mp4"
+  video=${x#*$q}; video=${video#*http}; video=${video%%\\x3C*}
+  video=$(echo "$video" | sed "s/\&amp;/\&/g")
 
-if [ $? = 0 ]; then
-  rm "video.$r.mp4"
-  rm "audio.$r.aac"
-fi
+  wget -U Mozilla -O "$title.$q.mp4" "http$video"
+
+  break
+done
+
+
