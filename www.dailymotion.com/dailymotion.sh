374db3132d35e345832fdc62182500f15977ac84
wei <wrenth04@gmail.com>
2017-11-28 11:05:22 +0800

    add dailymotion

---
+++ www.dailymotion.com/dailymotion.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+url=$1
+
+html=$(wget -U Mozilla -q -O - "$url")
+
+title=$(echo "$html" | grep "og:title")
+title=${title#*content=\"}; title=${title%%\"*}
+title=${title% - Dailymotion*}
+title=$(echo "$title" | sed "s/\//\./g")
+
+for q in "1280x720" "848x480"; do
+  video=$(echo "$html" | grep "$q")
+  if [ "x" = "x$video" ]; then continue; fi
+  video=${video##*$q}; video=${video%%\"*}
+  video=$(echo $video | sed "s/\\\\\\//\\//g")
+  break
+done
+
+wget -U Mozilla -O "$title.$q.mp4" "https://www.dailymotion.com/cdn/H264-1280x720$video"
