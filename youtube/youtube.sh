99b2c86ff5c08dcbdd7cacdf78f3e871370fd141
wei <wrenth04@gmail.com>
2017-11-29 09:59:08 +0800

    fix youtube title

--- youtube/youtube.sh
+++ youtube/youtube.sh
@@ -4,7 +4,9 @@ id=$1
 
 data=$(wget -U Mozilla -q -O - "http://www.youtube.com/get_video_info?eurl=http%3A%2F%2Fkej.tw%2F&sts=17316&video_id=$id")
 
+
 node decode.js "$data" | grep "H.264" | grep "AAC" | while read url title o; do
+  title=$(echo "$title" | sed "s/\//\./g" | sed "s/\+/ /g")
   wget -U Mozilla -O "$title.mp4" "$url"
   break
 done
