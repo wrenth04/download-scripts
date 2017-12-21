6df6f62311f95290174757d67640653e1fb167b9
wei <wrenth04@gmail.com>
2017-12-21 16:59:39 +0800

    fix name

--- ffmpeg/9pic.sh
+++ ffmpeg/9pic.sh
@@ -2,12 +2,12 @@
 
 name="$1"
 
-t=$(ffmpeg -i DV1231.avi 2>&1| grep Duration )
+t=$(ffmpeg -i "$name" 2>&1| grep Duration )
 hh=${t#*: }; mm=${hh#*:}; ss=${mm#*:}
 hh=${hh%%:*}; mm=${mm%%:*}; ss=${ss%%.*}
 
 frame=$((hh*3600*25 + mm*60*25 + ss*25))
 delay=$((frame / 10))
-s=$((delay/2))
+offset=$((delay/2))
 
-ffmpeg -i "$name" -frames 1 -vf "select=not(mod($s+n\,$delay)),scale=320:180,tile=3x3" "${name%.*}.png"
+ffmpeg -i "$name" -frames 1 -vf "select=not(mod($offset+n\,$delay)),scale=320:180,tile=3x3" "${name%.*}.png"
