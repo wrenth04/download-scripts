b5595ab9f382d2b7b0783ac62d3c6a13bf22ab67
wei <wrenth04@gmail.com>
2017-12-21 17:11:00 +0800

    add size param

--- ffmpeg/9pic.sh
+++ ffmpeg/9pic.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 name="$1"
+size="$2" # 3x3
+
+if [ "x" = "x$size" ]; then size="3x3"; fi
+
+n1=${size%x*}; n2=${size#*x}
+n=$((n1 * n2 + 1))
 
 t=$(ffmpeg -i "$name" 2>&1| grep Duration )
 hh=${t#*: }; mm=${hh#*:}; ss=${mm#*:}
 hh=${hh%%:*}; mm=${mm%%:*}; ss=${ss%%.*}
 
 frame=$((hh*3600*25 + mm*60*25 + ss*25))
-delay=$((frame / 10))
+delay=$((frame / n))
 offset=$((delay/2))
 
-ffmpeg -i "$name" -frames 1 -vf "select=not(mod($offset+n\,$delay)),scale=320:180,tile=3x3" "${name%.*}.png"
+ffmpeg -i "$name" -frames 1 -vf "select=not(mod($offset+n\,$delay)),scale=320:180,tile=$size" "${name%.*}.$size.png"
