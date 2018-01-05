2fd1ac12ab9a38b2a205085307143d4f0e42307a
Phil Huang <wrenth04@gmail.com>
2018-01-05 11:19:21 +0800

    update openload

--- openload/openload.js
+++ openload/openload.js
@@ -2,7 +2,15 @@ const Chromeless = require('chromeless').Chromeless;
 const url = process.argv[2];
 
 new Chromeless({launchChrome: false}).goto(url)
-  .evaluate(() => 'https://openload.co/stream/'+$('#streamuri').text())
+  .evaluate(() => {
+    var video;
+    try {
+      video = 'https://openload.co/stream/'+$('#streamurl, #streamuri, #streamurj').text();
+    } catch(e) {
+      video = 'video';
+    }
+    return video.indexOf('undefined') == -1 ? video : 'video';
+   })
   .end()
   .then(console.log);
 
