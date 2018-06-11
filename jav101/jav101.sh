fc8f10243e250853a18f17a94bdd68197f027d9b
wei <wrenth04@gmail.com>
2018-06-11 11:46:50 +0800

    fix jav101

--- jav101/jav101.sh
+++ jav101/jav101.sh
@@ -16,17 +16,26 @@ m3u8=$(wget -q -O - -U Mozilla --load-cookies=cookie "$m3u8" | tr -d '\r')
 echo $title
 name="$title.$id.mp4"
 
+#echo "$m3u8" | grep ts | while read u; do
+#  j=$((i+10000)); j=${j#1}
+#  i=$((i+1))
+#
+#  echo -ne "\r$j"
+#
+#  #wget -q -O "$id-$j.ts" -U Mozilla --load-cookies=cookie "https://svt.jav101.com/$id/$u"
+#done
+
 i=0
-echo "$m3u8" | grep ts | while read u; do
+while :; do
   j=$((i+10000)); j=${j#1}
-  i=$((i+1))
-
   echo -ne "\r$j"
-
-  wget -q -O "$id-$j.ts" -U Mozilla --load-cookies=cookie "https://svt.jav101.com/$id/$u"
+  wget -q -O "$id-$j.ts" -U Mozilla --load-cookies=cookie "https://svt.jav101.com/$id/ts/$id-$i.ts"
+  if [ $? != 0 ]; then break; fi
+  i=$((i+1))
 done
-
 cat $id*.ts > $id.tmp
 ffmpeg -y -i $id.tmp -c copy "$name"
 
 #ffmpeg -y -i "$m3u8" -headers "User-Agent: Mozilla" -headers "$(cat ffmpeg.cookie)\r\n" "$name"
+
+rm $id*
