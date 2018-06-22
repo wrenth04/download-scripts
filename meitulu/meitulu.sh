36abb1aeb55d9785d43881579642189eeed9f01a
wei <wrenth04@gmail.com>
2018-06-22 11:35:32 +0800

    add meitulu

---
+++ meitulu/meitulu.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+url=$1
+id=${url#*/t/}; id=${id%%/*}
+#html=$(wget -U Mozilla -q -O - "$url" | gzip -d)
+
+html=$(cat page.html)
+html=${html#*listtags}
+
+x=${html}
+x2=${x#*<li>}
+
+while [ "$x" != "$x2" ]; do
+  url=${x2#*href=\"}; url=${url%%\"*}
+  title=${x2#*p_title}; title=${title#*blank\">}; title=${title%%<*}
+  echo "$url $title"
+
+  x=${x2}
+  x2=${x#*<li>}
+done | while read url title; do
+  echo "Download $url $title"
+  if [ -e "$title" ]; then continue; fi
+
+  mkdir "$title"
+  aid=${url#*/item/}; aid=${aid%%.html}
+  i=1
+  while :; do
+    echo -ne "\r$i..."
+    output="$title/$title-$i.jpg"
+    wget -U Mozilla --referer "$url" -O "$output" -q "https://mtl.ttsqgs.com/images/img/$aid/$i.jpg"
+    i=$((i+1))
+    e=$(ls -l "$output" | grep " 0 ")
+    if [ "x$e" != "x" ]; then 
+      rm "$output"
+      break
+    fi
+  done
+done
