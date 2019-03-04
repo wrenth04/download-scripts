e888eac02a47df6a7b6b7a0b190e32771c0ac987
wei <wrenth04@gmail.com>
2019-03-04 16:40:53 +0800

    update igg

--- igg/igg.sh
+++ igg/igg.sh
@@ -7,19 +7,17 @@ title=${html#*title>}; title=${title%%<*}; title=${title%%Free Download*}
 
 echo "download $title"
 
-links=${html#*Google Drive}; links=${links%%p>*}
-
-x=${links}
-x2=${x#*id=}
+x=${html}
+x2=${x#*file/d/}
 
 while [ ! "$x" = "$x2" ]; do
   #link=${x2%%\"*}
   #html=$(wget --content-on-error -O - -q -U Mozilla --referer "$url" "$link")
   #id=${x2#*id=};
-  id=${x2%%&*}
+  id=${x2%%/*}
   gdrive download $id
 
   x=${x2}
-  x2=${x#*id=}
+  x2=${x#*file/d/}
 done
 
