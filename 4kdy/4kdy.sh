94220510d16d733d9f3863bceb0b3fb41f459dd7
wei <wrenth04@gmail.com>
2019-03-19 21:49:51 +0800

    add 4kdy

---
+++ 4kdy/4kdy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+AGENT="Mozilla"
+url="$1"
+
+html=$(wget -U "AGENT" -q -O - "$url")
+
+x=${html#*content__playlist}
+x=${x%%ul*}
+x2=${x#*href=\'}
+
+while [ "$x" != "$x2" ]; do
+  link="https://www.4kdy.net${x2%%\'*}"
+  echo $link
+  x=${x2}
+  x2=${x#*href=\'}
+done | while read link; do
+  html=$(wget -U "AGENT" -q -O - "$link")
+  m3u8=$(echo "$html" | grep m3u8)
+  m3u8=${m3u8#*\'}; m3u8=${m3u8%\'*}
+  title=$(echo "$html" | grep "text-muted")
+  title=${title%%</a*}; title=${title##*>}
+  title=$(echo "$title" | sed 's/\//\./g')
+  name="$title.mp4"
+  if [ -e "$name" ]; then continue; fi
+  echo $name
+  echo $m3u8
+  ffmpeg -y -nostdin -i $m3u8 -c copy "$title.inprogress.mp4"
+  mv "$title.inprogress.mp4" "$name"
+done
