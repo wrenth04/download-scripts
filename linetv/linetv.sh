a846329bec39369f9aabbc18f5364b0eb1afe951
黃威人 <wrenth04@gmail.com>
2019-04-29 11:09:12 +0800

    todo: linetv

--- linetv/linetv.sh
+++ linetv/linetv.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"
+
 url=$1
 
-id=${url#*/v/}; id=${id%%_*}
-html=$(wget -q -O - -U Mozilla "$url")
-title=${html#*title>}; title=${title%%<*}
-title=$(echo "$title" | sed "s/ | /\./" | sed "s/\//\./g")
-key=${html#*key: \'}; key=${key%%\'*}
-json=$(wget -q -O - -U Mozilla "https://tv.line.me/api/mobile/video/play/$id?key=$key")
-video=${json#*height\":1080}; video=${video#*source\":\"}; video=${video%%\"*}
-
+html=$(wget -q -O - -U "$AGENT" "$url")
+drama_id=${html#*drama_id\":\"}; drama_id=${drama_id%%\"*}
+eps=${html#*currentEps\":\"}; eps=${eps%%\"*}
+title=${html#*VideoObject}; title=${title#*name\": \"}; title=${title%%\"*}
+title=$(echo "$title" | sed 's/\//\./g')
+json=$(wget -q -O - -U "$AGENT" "www.linetv.tw/api/part/$drama_id/eps/$eps/part")
+m3u8=${json%%\.m3u8*}; m3u8=${m3u8##*\"}
 echo "download $title.mp4"
-wget -O "$title.mp4" -U Mozilla --referer "$url" "$video"
+echo $m3u8
+ffmpeg -nostdin -i "$m3u8.m3u8" -c copy "$title.mp4"
