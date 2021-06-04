68c08e26491cd9cb38b376d6f174c6c075d6db1b
wei <wrenth04@gmail.com>
2021-06-04 09:54:03 +0000

    update 567pic

--- 567pic/567pic.sh
+++ 567pic/567pic.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 url="$1"
+start_cid="$2"
+start_cid=${start_cid:-0}
 
 html=$(wget -q -O - "$url")
 title=${html#*title>}; title=${title%%<*}
@@ -9,7 +11,11 @@ title=${title%-*}
 echo "title: $title"
 mkdir "$title"
 x=${html#*playlist}
+cid=0
 echo "$x" | grep btn | while read n; do 
+  cid=$((cid+1))
+  if [ $cid -lt $start_cid ]; then continue; fi
+
   chapter=${n#*>}; chapter=${chapter%<*}
   mkdir "$title/$chapter"
   i=1
